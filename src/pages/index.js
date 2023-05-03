35fb366583c6544ee2237b96a7e0d7696d4ad2ef
luanavfg <luanavefago1@gmail.com>
2023-05-03 11:29:25 -0300

    feat: class 01 - create page with getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
-const Home = () => {
-  return <p>Hello, egghead</p>;
+const Home = ({ quote }) => {
+  return <p>{JSON.stringify(quote)}</p>;
 };
 
+export const getStaticProps = async () => {
+  // fetch data
+  const quote = await fetch("https://zenquotes.io/api/today").then((resp) =>
+    resp.json()
+  );
+
+  return {
+    props: {
+      quote,
+    },
+  };
+};
 export default Home;
